a42317989d6a97368a054f588b8f4cad4592b7d6
Antonio Victor Ribeiro Morelli <victor.morelli.007@gmail.com>
2021-01-31 17:46:23 -0300

    number_exercises complete, all methods working

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -1,36 +1,42 @@
-def add(a, b)
-  # return the result of adding a and b
-  a + b
+# teste
+
+def add(first_a, first_b)
+  first_a + first_b
 end
 
-def subtract(a, b)
-  # return the result of subtracting b from a
+def subtract(first_a, first_b)
+  first_a - first_b
 end
 
-def multiply(a, b)
-  # return the result of multiplying a times b
+def multiply(first_a, first_b)
+  first_a * first_b
 end
 
-def divide(a, b)
-  # return the result of dividing a by b
+def divide(first_a, first_b)
+  first_a / first_b
 end
 
-def remainder(a, b)
+def remainder(first_a, first_b)
   # return the remainder of dividing a by b using the modulo operator
+  first_a % first_b
 end
 
-def float_division(a, b)
+def float_division(first_a, first_b)
   # return the result of dividing a by b as a float, rather than an integer
+  first_a / first_b.to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  string.to_i
 end
 
 def even?(number)
   # return true if the number is even (hint: use integer's even? method)
+  number.even?
 end
 
 def odd?(number)
   # return true if the number is odd (hint: use integer's odd? method)
+  number.odd?
 end
